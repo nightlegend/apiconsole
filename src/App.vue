bd2062819fc6448a79c39ed0ac0cb26692ba1dea
David guo <811745733@qq.com>
2017-03-21 22:48:31 +0800

    update index page info.

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,66 @@
 <template>
   <div>
     <el-menu theme="dark" :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
-      <el-menu-item index="1">处理中心</el-menu-item>
+      <el-menu-item index="1">Api Center</el-menu-item>
       <el-submenu index="2">
-        <template slot="title">我的工作台</template>
+        <template slot="title">我的api</template>
         <el-menu-item index="2-1">选项1</el-menu-item>
         <el-menu-item index="2-2">选项2</el-menu-item>
         <el-menu-item index="2-3">选项3</el-menu-item>
       </el-submenu>
-      <el-menu-item index="3"><a href="#" target="_blank">订单管理</a></el-menu-item>
+      <el-menu-item index="3"><a href="#" target="_blank">个人信息管理</a></el-menu-item>
     </el-menu>
     <div class="line"></div>
+
     <el-carousel :interval="4000" type="card" height="500px">
       <el-carousel-item v-for="item in 6">
         <h3>{{ item }}</h3>
       </el-carousel-item>
     </el-carousel>
+    <el-row :gutter="20">
+      <el-col :span="12" :offset="6"><div class="grid-content lg-purple">
+        <el-input
+              placeholder="请输入关键字"
+              icon="search"
+              v-model="input2"
+              :on-icon-click="handleIconClick" >
+            </el-input>
+      </div></el-col>
+    </el-row>
+
+
+    <el-row class="bar">
+      <el-col :span="8" v-for="(o, index) in 5" :offset="index > 0 ? 2 : 2">
+        <el-card :body-style="{ padding: '20px' }" class="eachcard">
+          <div style="padding: 24px;">
+            <span>时间你好。。。。。。</span>
+            <div class="bottom clearfix">
+              <time class="time">{{ currentDate }}</time>
+              <el-button type="text" class="button">操作按钮</el-button>
+            </div>
+          </div>
+        </el-card>
+      </el-col>
+    </el-row>
+
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app'
+  data () {
+    return {
+      name: 'app',
+      currentDate: new Date(),
+      input2: ''
+    }
+  },
+  methods: {
+    handleIconClick (ev) {
+      console.log(ev)
+    }
+  }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-.el-carousel__item h3 {
-    color: #475669;
-    font-size: 14px;
-    opacity: 0.75;
-    line-height: 200px;
-    margin: 0;
-  }
-
-  .el-carousel__item:nth-child(2n) {
-    background-color: #99a9bf;
-  }
-
-  .el-carousel__item:nth-child(2n+1) {
-    background-color: #d3dce6;
-  }
-</style>
+<style src="./assets/css/app.css" scoped></style>
